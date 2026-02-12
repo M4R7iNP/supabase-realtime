8f5b6734b521565f763b50a5bad04c6be4c9da58
Martin Pedersen <martin@m4r7.in>
2026-02-12 23:15:26 +0100

    wip

--- lib/extensions/postgres_cdc_rls/subscriptions.ex
+++ lib/extensions/postgres_cdc_rls/subscriptions.ex
@@ -211,17 +211,22 @@ defmodule Extensions.PostgresCdcRls.Subscriptions do
   def parse_subscription_params(params) do
     case params do
       %{"schema" => schema, "table" => table, "filter" => filter} ->
-        with [col, rest] <- String.split(filter, "=", parts: 2),
-             [filter_type, value] when filter_type in @filter_types <-
-               String.split(rest, ".", parts: 2),
-             {:ok, formatted_value} <- format_filter_value(filter_type, value) do
-          {:ok, {schema, table, [{col, filter_type, formatted_value}]}}
-        else
-          {:error, msg} ->
-            {:error, "Error parsing `filter` params: #{msg}"}
-
-          e ->
-            {:error, "Error parsing `filter` params: #{inspect(e)}"}
+        try do
+          {:ok,
+           {schema, table,
+            Enum.map(String.split(filter, ","), fn part ->
+              with [col, rest] <- String.split(part, "=", parts: 2),
+                   [filter_type, value] when filter_type in @filter_types <-
+                     String.split(rest, ".", parts: 2),
+                   {:ok, formatted_value} <- format_filter_value(filter_type, value) do
+                {col, filter_type, formatted_value}
+              else
+                {:error, msg} -> throw("Error parsing `filter` params: #{msg}")
+                e -> throw("Error parsing `filter` params: #{inspect(e)}")
+              end
+            end)}}
+        catch
+          msg -> {:error, msg}
         end
 
       %{"schema" => schema, "table" => table} ->
